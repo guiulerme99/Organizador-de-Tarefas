ff04dc2f83faa5941b6321baa6c3166fa384dcd7
guiulerme99 <guihrsilva99@gmail.com>
2025-04-16 20:18:10 -0300

    (IgorG) Criando botao e melhorando design

--- site/script.js
+++ site/script.js
@@ -1,7 +1,20 @@
-function adicionar() {
+function adicionar(x) {
     document.getElementById("adicionar").style.display = "flex"
+    if (x==1) {
+        document.getElementById("valor-titulo").innerHTML = "À Fazer"
+    } else if (x==2) {
+        document.getElementById("valor-titulo").innerHTML = "Em andamento"
+    } else if (x==3) {
+        document.getElementById("valor-titulo").innerHTML = "Concluido"
+    } else {
+        cartao(x);
+    }
 }
 
 function fechar() {
     document.getElementById("adicionar").style.display = "none"
+}
+
+function cartao(y) {
+    
 }
